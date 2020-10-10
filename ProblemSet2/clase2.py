ffa303870f8736ac515714fc7ec81750c2c5c52f
matisel95 <matias@almafintech.com.ar>
2020-10-09 21:03:48 -0300

    fixed

--- ProblemSet2/clase2.py
+++ ProblemSet2/clase2.py
@@ -15,29 +15,19 @@ class Portfolio():
 
     def __init__(self, daily_ret=[]):
         self.assets = daily_ret
+        self.assetcount = daily_ret.shape[1]
 
     def get_balanced_portfolio(self,daily_factors=[]):
-
         retTuple = ([],[])
         min_MSE = 1
 
-        self.assets = self.assets.iloc[:,1:]
 
-        rf = daily_factors.iloc[:,-1:]
-        rf = rf['RF']
-        rf = rf.reset_index(drop=True)
+        df = daily_factors.join(self.assets)
+        X = df.iloc[:,0:daily_factors.shape[1]-1]
 
-        daily_factors = daily_factors.iloc[:,1:-1]
-        X = daily_factors
-        X = X.reset_index(drop=True)
-
-        alloc = utils.get_allocations(self.assets.shape[1])
-        for weights in alloc:
-            Y = self.assets.dot(weights)
-            Y = Y.reset_index(drop=True)
-
-            Y_P = 100*Y.subtract(rf)
-            regression = sk_lm.LassoCV(cv=5).fit(X, Y_P)
+        for weights in utils.get_allocations(self.assets.shape[1]):
+            Y = df.iloc[:, daily_factors.shape[1]:] @ weights
+            regression = sk_lm.LassoCV(cv=5).fit(X, Y)
             MSE = mean_squared_error(np.ones(X.shape[1])/X.shape[1], regression.coef_)
             if MSE < min_MSE:
                 retTuple = (weights,regression.coef_)
@@ -46,25 +36,11 @@ class Portfolio():
         return retTuple
 
 if __name__ == '__main__':
-    df_etfs = pd.read_csv('selected_etfs.csv')
-    df_etfs = df_etfs.dropna()
+    df_etfs = pd.read_csv('selected_etfs.csv',index_col=0,parse_dates=True).pct_change()
+    df_FF = pd.read_csv('F-F_Research_Data_5_Factors_2x3_daily.csv',index_col=0,parse_dates=True)/100
 
-    date = df_etfs.iloc[:,0]
-    df_etfs = df_etfs.pct_change()
-    df_etfs.iloc[:,0] = date
-
-    df_FF = pd.read_csv('F-F_Research_Data_5_Factors_2x3_daily.csv')
-
-    #   Elijo solo 'DATE','BOND','SUSA','DNL','XLF','XSLV'
-    df_etfs = df_etfs[['DATE','BOND','SUSA','DNL','XLF','XSLV']]
-
-    #   Elijo solo año 2017
-    df_FF = df_FF[(df_FF['Date'] >= 20170000) & (df_FF['Date'] < 20180000)]
-    df_etfs = df_etfs[(df_etfs['DATE'] >= 20170000) & (df_etfs['DATE'] < 20180000)]
-
-
-    myPort = Portfolio(df_etfs)
-    weigths, exposure = myPort.get_balanced_portfolio(df_FF)
+    myPort = Portfolio(df_etfs.loc['2017',['BOND','SUSA','DNL','XLF','XSLV']])
+    weigths, exposure = myPort.get_balanced_portfolio(df_FF.loc['2017'])
 
     print("--- Portfolio Balanceado ---")
     print("Weights  : ", end = '')
